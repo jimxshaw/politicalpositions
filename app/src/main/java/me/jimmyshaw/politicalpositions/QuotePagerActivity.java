7f3307e7bd44eb4f9e728b8c2e0fda2b180befb9
Jim Shaw <jimxshaw@gmail.com>
2016-04-11 17:06:54 -0500

    added an id field to Quote model class, refactored IssueLab class accordingly. added layout for and class for Quote ViewPager activity, partially implementing the activity.

---
+++ app/src/main/java/me/jimmyshaw/politicalpositions/QuotePagerActivity.java
@@ -0,0 +1,59 @@
+package me.jimmyshaw.politicalpositions;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+// Every issue has a list of quotes. Our QuotePagerActivity will host a list of QuoteFragments and
+// allows us to page through the quotes about the selected issue.
+public class QuotePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_ISSUE_ID = "issue_id";
+
+    private ViewPager mViewPager;
+    private List<Quote> mQuotes;
+
+    public static Intent newInstance(Context packageContext, int issueId) {
+        Intent intent = new Intent(packageContext, QuotePagerActivity.class);
+        intent.putExtra(EXTRA_ISSUE_ID, issueId);
+        return intent;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote_pager);
+
+        int issueId = (int) getIntent().getSerializableExtra(EXTRA_ISSUE_ID);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_quote_pager_view_pager);
+
+        mQuotes = IssueLab.get(this).getIssue(issueId).getQuotes();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+
+
+                // TODO: finish implementing getItem method
+
+
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return mQuotes.size();
+            }
+        });
+
+    }
+}
