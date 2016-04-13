3114c208d5bb22742f12aaa06ac7310482e98b95
Jim Shaw <jimxshaw@gmail.com>
2016-04-13 10:41:38 -0500

    within QuoteFragment, found views by id, cast them, assigned data retrieved to the views

--- app/src/main/java/me/jimmyshaw/politicalpositions/QuoteFragment.java
+++ app/src/main/java/me/jimmyshaw/politicalpositions/QuoteFragment.java
@@ -2,6 +2,10 @@ package me.jimmyshaw.politicalpositions;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class QuoteFragment extends Fragment {
 
@@ -10,6 +14,12 @@ public class QuoteFragment extends Fragment {
 
     private Issue mIssue;
     private Quote mQuote;
+
+    private TextView mQuoteTitle;
+    private TextView mQuoteCandidate;
+    private TextView mQuoteBody;
+    private TextView mQuoteSource;
+    private TextView mQuoteDate;
 
     public static QuoteFragment newInstance(int issueId, int quoteId) {
         Bundle args = new Bundle();
@@ -27,5 +37,30 @@ public class QuoteFragment extends Fragment {
         int issueId = (int) getArguments().getSerializable(ARG_ISSUE_ID);
         int quoteId = (int) getArguments().getSerializable(ARG_QUOTE_ID);
         mQuote = IssueLab.get(getActivity()).getIssue(issueId).getQuote(quoteId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_quote, container, false);
+
+        mQuoteTitle = (TextView) view.findViewById(R.id.issue_quote_title);
+        mQuoteTitle.setText(mIssue.getTitle());
+
+        mQuoteCandidate = (TextView) view.findViewById(R.id.issue_quote_candidate);
+        mQuoteCandidate.setText(mQuote.getCandidate());
+
+        mQuoteBody = (TextView) view.findViewById(R.id.issue_quote_body);
+        mQuoteBody.setText(mQuote.getQuote());
+
+        // TODO: Refactor the string source into an actual URL.
+        mQuoteSource = (TextView) view.findViewById(R.id.issue_quote_source);
+        mQuoteSource.setText(mQuote.getSource().toString());
+
+        mQuoteDate = (TextView) view.findViewById(R.id.issue_quote_date);
+        mQuoteDate.setText(mQuote.getDate().toString());
+
+        
+        return view;
     }
 }
