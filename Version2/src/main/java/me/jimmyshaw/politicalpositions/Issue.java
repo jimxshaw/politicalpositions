50942e1ad526b7ebf5cbfbca2426f46a48de0f8c
Jim Shaw <jimxshaw@gmail.com>
2016-05-02 15:05:48 -0500

    added Issue and Quote model classes

---
+++ Version2/src/main/java/me/jimmyshaw/politicalpositions/Issue.java
@@ -0,0 +1,59 @@
+package me.jimmyshaw.politicalpositions;
+
+import java.util.ArrayList;
+
+public class Issue {
+    private int mId;
+    private String mTitle;
+    private ArrayList<Quote> mQuotes;
+
+    public Issue() {
+        mQuotes = new ArrayList<>();
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Quote getQuote(int quoteId) {
+        for (Quote quote : mQuotes) {
+            if (quote.getId() == quoteId) {
+                return quote;
+            }
+        }
+        return null;
+    }
+
+    public Quote getQuoteByCandidate(String candidateName) {
+        for (Quote quote : mQuotes) {
+            if (quote.getCandidate().equals(candidateName)) {
+                return quote;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Quote> getQuotes() {
+        return mQuotes;
+    }
+
+    public void setQuotes(ArrayList<Quote> quotes) {
+        mQuotes = quotes;
+    }
+
+    public int quotesCount() {
+        return mQuotes.size();
+    }
+}
