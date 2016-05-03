31ff6368d78aa59fcf221dfa5a62165c15022e36
James <jimxshaw@gmail.com>
2016-05-03 11:29:22 -0500

    implemented QuotePagerActivity, created boilerplate QuoteFragment

---
+++ Version2/src/main/java/me/jimmyshaw/politicalpositions/QuoteFragment.java
@@ -0,0 +1,11 @@
+package me.jimmyshaw.politicalpositions;
+
+import android.support.v4.app.Fragment;
+
+public class QuoteFragment {
+
+    public static Fragment newInstance(int issueId, int quoteId) {
+
+        return null;
+    }
+}
