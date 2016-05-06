559cd183e010439c3ddbb7a78130f1ecc3e4cef8
James <jimxshaw@gmail.com>
2016-05-06 16:06:32 -0500

    modified layout for menu item about page

--- Version2/src/main/java/me/jimmyshaw/politicalpositions/IssueListActivity.java
+++ Version2/src/main/java/me/jimmyshaw/politicalpositions/IssueListActivity.java
@@ -93,18 +93,19 @@ public class IssueListActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        super.onOptionsItemSelected(menuItem);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (menuItem.getItemId()) {
+            case R.id.action_about_page:
+
+                break;
+            default:
+
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
